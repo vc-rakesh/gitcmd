5f874d93eebf71aae4fc004529856c5aff3497d0
Rakesh Sojitra <rakesh.sojitra@viitor.cloud>
2023-05-11 12:07:41 +0530

    fix: addition function
    - fix addition from multiple in calc file
    
    RefBy: fffsdsf

--- calc.js
+++ calc.js
@@ -11,8 +11,6 @@ let result;
 
 // using if...else if... else
 if (operator === "+") {
-  result = number1 * number2;
-} else if (operator === "-") {
   result = number1 + number2;
 } else if (operator === "*") {
   result = number1 / number2;
